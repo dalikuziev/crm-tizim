09fcc6d3f80649ab0265b314be3a623d8e820623
diyorbek <dalikuziev@gmail.com>
2026-08-16 21:41:40 +0500

    2026-08-16

--- course/admin.py
+++ course/admin.py
@@ -1,21 +1,21 @@
 from django.contrib import admin
 from .models import Subject, Teacher, Student
-from import_export.admin import ImportExportActionModelAdmin
+from import_export.admin import ImportExportModelAdmin
 
 @admin.register(Subject)
-class SubjectAdmin(ImportExportActionModelAdmin):
+class SubjectAdmin(ImportExportModelAdmin):
     list_display = ('title',)
     search_fields = ('title',)
     list_filter = ('title',)
 
 @admin.register(Teacher)
-class TeacherAdmin(ImportExportActionModelAdmin):
+class TeacherAdmin(ImportExportModelAdmin):
     list_display = ('first_name', 'last_name', 'phone_number', 'birth_date',)
     search_fields = ('first_name', 'last_name')
     list_filter = ('first_name', 'last_name')
 
 @admin.register(Student)
-class StudentAdmin(ImportExportActionModelAdmin):
+class StudentAdmin(ImportExportModelAdmin):
     list_display = ('first_name', 'last_name', 'phone_number', 'time',)
     search_fields = ('first_name', 'last_name', 'phone_number', 'time',)
     list_filter = ('first_name', 'last_name', 'phone_number', 'time',)
