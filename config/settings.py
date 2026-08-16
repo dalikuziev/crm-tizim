b90cd12e61bd6f4e334aee0302eae409ca4926fb
diyorbek <dalikuziev@gmail.com>
2026-08-16 21:25:45 +0500

    2026-08-16

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,3 @@
-import os
 from pathlib import Path
 from environs import Env
 env = Env()
@@ -107,9 +106,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-# STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
