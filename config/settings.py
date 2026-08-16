7a8c372fd21c1d8406e8dfb070b2909d6d65fd35
diyorbek <dalikuziev@gmail.com>
2026-08-16 21:27:35 +0500

    2026-08-16

--- config/settings.py
+++ config/settings.py
@@ -14,12 +14,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
-
-# Application definition
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
